3576ae7464d61ed8ffce6e0248360161e511ab73
Chathindu Bandara <73778033+ChathinduN@users.noreply.github.com>
2024-10-19 23:16:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ try:
                 file.write(f"""
                 <div class="post">
                     <h2 class="title"><a href="{post_url}" target="_blank">{submission.title}</a></h2>
-                    <p class="author">Posted by: {submission.author}</p>
+                    <p class="author">Posted by: <a href="https://reddit.com/u/{submission.author}>{submission.author}</a></p>
                     <div class="content">{content}</div>
                     <p class="timestamp" hidden>Timestamp: {submission.created_utc}</p>
                 </div>
